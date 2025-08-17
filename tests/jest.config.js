f674764921e0da51cba455a993cabdbc31f1eead
sleepingeraser <windyandmelody2005@gmail.com>
2025-08-17 20:35:59 +0800

    updated

--- tests/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  setupFilesAfterEnv: ["./tests/setup.js"],
-  coveragePathIgnorePatterns: ["/node_modules/", "/config/"],
-
-  testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.js", "**/?(*.)+(spec|test).js"],
-};
